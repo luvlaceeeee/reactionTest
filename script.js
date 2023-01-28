a2823284b4f519970117e7b00636faf54c8985e6
luvlaceeeee <nikitan2002@gmail.com>
2023-01-29 01:43:55 +0300

    need fix promise for timer

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const startButton = document.querySelector('#start-button')
 const mainForm = document.querySelector('.container')
+
 startButton.addEventListener('click', () => {
-    mainForm.style.display = 'none'
+    // mainForm.style.display = 'none'
+    mainForm.hidden = true
     document.body.style.background = 'linear-gradient(-45deg, #ee5252, #e73c56)'
 
     const divForReadyText = document.createElement('div')
@@ -27,6 +29,27 @@ startButton.addEventListener('click', () => {
         }
     }
 
+    //TODO Разобрать, как отображать надпись wait после таймера(скорее всего юзать промисы)
+
+    // function getTimer(){
+    //     return new Promise((resolve, reject) => {
+    //         let flag = false
+    //         let timer3sec = setInterval(timer(readyTimer.textContent - 1), 1000)
+    //         setTimeout(() => { clearInterval(timer3sec)
+    //             flag = true }, ((readyTimer.textContent - 1) * 1000));
+    //         if (flag){
+    //             resolve(flag)
+    //         }
+    //     })
+    // }
+    //
+    // getTimer()
+
     let timer3sec = setInterval(timer(readyTimer.textContent - 1), 1000)
     setTimeout(() => { clearInterval(timer3sec)}, ((readyTimer.textContent - 1) * 1000));
+
+        // divForReadyText.style.height = '70px'
+        // getReadyText.textContent = 'Wait...'
+        // readyTimer.hidden = true
+        // brEl.hidden = true
 })
