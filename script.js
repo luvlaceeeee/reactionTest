b2347bf1256edc24dd66b03fc02aa07387d9012b
luvlaceeeee <nikitan2002@gmail.com>
2023-01-29 11:46:19 +0300

    fixed promise timer, first try for reaction test

--- script.js
+++ script.js
@@ -22,6 +22,10 @@ startButton.addEventListener('click', () => {
     readyTimer.classList = 'timer'
     divForReadyText.appendChild(readyTimer)
 
+    function getRandomInt(max) {
+        return Math.floor(Math.random() * max);
+    }
+
     function timer(num){
         return function (){
             readyTimer.textContent = num
@@ -29,27 +33,34 @@ startButton.addEventListener('click', () => {
         }
     }
 
-    //TODO Разобрать, как отображать надпись wait после таймера(скорее всего юзать промисы)
+    function timeout (ms) {
+        return new Promise(res => setTimeout(res,ms));
+    }
 
-    // function getTimer(){
-    //     return new Promise((resolve, reject) => {
-    //         let flag = false
-    //         let timer3sec = setInterval(timer(readyTimer.textContent - 1), 1000)
-    //         setTimeout(() => { clearInterval(timer3sec)
-    //             flag = true }, ((readyTimer.textContent - 1) * 1000));
-    //         if (flag){
-    //             resolve(flag)
-    //         }
-    //     })
-    // }
-    //
-    // getTimer()
+    async function intervalTimer(){
+        let timer3sec = setInterval(timer(readyTimer.textContent - 1), 1000)
+        setTimeout(() => { clearInterval(timer3sec)}, ((readyTimer.textContent - 1) * 1000));
+        await timeout(readyTimer.textContent * 1000)
+    }
 
-    let timer3sec = setInterval(timer(readyTimer.textContent - 1), 1000)
-    setTimeout(() => { clearInterval(timer3sec)}, ((readyTimer.textContent - 1) * 1000));
+    intervalTimer().then(() => {
+        divForReadyText.style.height = '70px'
+        getReadyText.textContent = 'Wait...'
+        readyTimer.hidden = true
+        brEl.hidden = true
+        // const startTime = Date.now()
+        // document.body.addEventListener('click', () => {
+        //     const endTime = Date.now()
+    }).then(() => {
+        setTimeout(() => {
+            const startTime = Date.now()
+            document.body.style.background = 'linear-gradient(-45deg, #23d526, #23d585)'
+            getReadyText.textContent = 'CLICK!'
+            document.body.addEventListener('click', () => {
+                const endTime = Date.now()
+                getReadyText.textContent = `Your result ${endTime - startTime} ms`
+            })
+        }, getRandomInt(7) * 1000)
+    })
 
-        // divForReadyText.style.height = '70px'
-        // getReadyText.textContent = 'Wait...'
-        // readyTimer.hidden = true
-        // brEl.hidden = true
 })
